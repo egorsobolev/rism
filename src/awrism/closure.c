3902ee3340abf124c21cf8f0a36fed630841b82d
Egor Sobolev <egorsobolev@users.noreply.github.com>
2026-01-11 13:11:05 +0100

    OpenMP parallelization of closure equations

--- src/awrism/closure.c
+++ src/awrism/closure.c
@@ -5,34 +5,37 @@
 
 void hnc(rismaw_t *p, const double *tuv, double *cuv, float *dcdt, double *en)
 {
-	int i, j, l, k, u, v, incu;
+	int i, j, l, k, u, v, incu, t;
+	double eni;
 
 	incu = p->natv * p->ge.n;
 
-	l = 0;
-	k = 0;
+	eni = 0;
+	#pragma omp single nowait
 	*en = 0;
+	#pragma omp for collapse(2)
 	for (u = 0; u < p->natu; u++) {
-		j = p->puv.atyp[u] * incu;
 		for (v = 0; v < p->natv; v++) {
+			j = p->puv.atyp[u] * incu + v * p->puv.ngrid;
+			t = (v + p->natv * u);
 			for (i = 0; i < p->gj.n; i++) {
+				k = i + p->gj.n * t;
+				l = i + p->ge.n * t;
 				cuv[l] = exp(tuv[l] - p->puv.u[j + i]) - 1.0;
 				dcdt[k] = (float) cuv[l];
-				*en += (cuv[l] + 1.0) * p->puv.u[j + i] * (i + 1) * (i + 1) * p->v.n[v];
+				eni += (cuv[l] + 1.0) * p->puv.u[j + i] * (i + 1) * (i + 1) * p->v.n[v];
 				cuv[l] -= tuv[l];
-				l++;
-				k++;
 			}
 			for (; i < p->ge.n; i++) {
+				l = i + p->ge.n * t;
 				cuv[l] = exp(tuv[l] - p->puv.u[j + i]) - 1.0;
-				*en += (cuv[l] + 1.0) * p->puv.u[j + i] * (i + 1) * (i + 1) * p->v.n[v];
+				eni += (cuv[l] + 1.0) * p->puv.u[j + i] * (i + 1) * (i + 1) * p->v.n[v];
 				cuv[l] -= tuv[l];
-				l++;
 			}
-			j += p->puv.ngrid;
 		}
 	}
-	*en *= p->puv.k;
+	#pragma atomic
+	*en += p->puv.k * eni;
 }
 void hnc_c(rismaw_t *p, const double *tuv, double *cuv)
 {
@@ -40,50 +43,52 @@ void hnc_c(rismaw_t *p, const double *tuv, double *cuv)
 
 	incu = p->natv * p->ge.n;
 
-	l = 0;
+	#pragma omp for collapse(2)
 	for (u = 0; u < p->natu; u++) {
-		j = p->puv.atyp[u] * incu;
 		for (i = 0; i < incu; i++) {
+			j = p->puv.atyp[u] * incu;
+			l = i + u * incu;
 			cuv[l] = exp(tuv[l] - p->puv.u[j + i]) - tuv[l] - 1.0;
-			l++;
 		}
 	}
 }
 
 void plhnc(rismaw_t *p, const double *tuv, double *cuv, float *dcdt, double *en)
 {
-	int i, j, l, k, u, v, incu;
-	double a;
+	int i, j, l, k, u, v, incu, t;
+	double a, eni;
 
 	incu = p->natv * p->ge.n;
 
-	l = 0;
-	k = 0;
+	eni = 0;
+	#pragma omp single nowait
 	*en = 0;
+	#pragma omp for collapse(2)
 	for (u = 0; u < p->natu; u++) {
-		j = p->puv.atyp[u] * incu;
 		for (v = 0; v < p->natv; v++) {
+			j = p->puv.atyp[u] * incu + v * p->puv.ngrid;
+			t = (v + p->natv * u);
 			for (i = 0; i < p->gj.n; i++) {
+				k = i + p->gj.n * t;
+				l = i + p->ge.n * t;
 				a = tuv[l] - p->puv.u[j + i];
 				cuv[l] = exp((a < 0.0) * a) - 1.0;
 				dcdt[k] = (float) cuv[l];
 				cuv[l] += (a >= 0.0) * a;
-				*en += (cuv[l] + 1.0) * p->puv.u[j + i] * (i + 1) * (i + 1) * p->v.n[v];
+				eni += (cuv[l] + 1.0) * p->puv.u[j + i] * (i + 1) * (i + 1) * p->v.n[v];
 				cuv[l] -= tuv[l];
-				l++;
-				k++;
 			}
 			for (; i < p->ge.n; i++) {
+				l = i + p->ge.n * t;
 				a = tuv[l] - p->puv.u[j + i];
 				cuv[l] = exp((a < 0.0) * a) - 1.0 + (a >= 0.0) * a;
-				*en += (cuv[l] + 1.0) * p->puv.u[j + i] * (i + 1) * (i + 1) * p->v.n[v];
+				eni += (cuv[l] + 1.0) * p->puv.u[j + i] * (i + 1) * (i + 1) * p->v.n[v];
 				cuv[l] -= tuv[l];
-				l++;
 			}
-			j += p->puv.ngrid;
 		}
 	}
-	*en *= p->puv.k;
+	#pragma omp atomic
+	*en += p->puv.k * eni;
 }
 
 void plhnc_c(rismaw_t *p, const double *tuv, double *cuv)
@@ -93,13 +98,13 @@ void plhnc_c(rismaw_t *p, const double *tuv, double *cuv)
 
 	incu = p->natv * p->ge.n;
 
-	l = 0;
+	#pragma omp for collapse(2)
 	for (u = 0; u < p->natu; u++) {
-		j = p->puv.atyp[u] * incu;
 		for (i = 0; i < incu; i++) {
+			j = p->puv.atyp[u] * incu;
+			l = i + u * incu;
 			a = tuv[l] - p->puv.u[j + i];
 			cuv[l] = exp((a < 0.0) * a) - 1.0 + (a >= 0.0) * a - tuv[l];
-			l++;
 		}
 	}
 }
